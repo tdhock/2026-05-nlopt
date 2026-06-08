43918c0d31ce64c69fa7c465f5d543443a759ca7
Toby Dylan Hocking <toby.hocking@r-project.org>
2026-06-08 13:24:15 -0400

    viz

---
+++ figure-armijo-2D-data.R
@@ -0,0 +1,146 @@
+library(data.table)
+library(animint2)
+f <- function(V1, V2){
+  (V1^2 + V2 - 11)^2 + (V1+V2^2-7)^2
+}
+xfun <- function(fun){
+  force(fun)
+  function(x)do.call(fun, as.list(x))
+}
+xf <- xfun(f)
+Vec <- function(to, from=c(0,0))data.table(to=t(to), from=t(from))
+grad <- function(x,y)c(
+  2*(x^2+y-11)*2*x+2*(x+y^2-7),
+  2*(x^2+y-11)+2*(x+y^2-7)*2*y)
+xg <- xfun(grad)
+hess <- function(x,y)rbind(
+  c(4*(x^2+y-11)+8*x^2+2, 4*x+4*y),
+  c(4*x+4*y, 4*(x+y^2-7)+8*y^2+2))
+xh <- xfun(hess)
+grad_at_d <- function(dir.vec)Q.mat %*% dir.vec + c.vec
+qd <- function(V1, V2){
+  V.mat <- cbind(V1, V2)
+  0.5*rowSums((V.mat %*% Q.mat) * V.mat) + V.mat %*% c.vec
+}
+xqd <- xfun(qd)
+L2norm <- function(x)sqrt(sum(x^2))
+
+gseq <- seq(-5, 5, by=0.1)
+(grid.dt <- CJ(V1=gseq, V2=gseq)[
+, log10.f := log10(f(V1,V2)+1)
+][])
+ggplot()+
+  coord_equal()+
+  geom_tile(aes(
+    V1, V2, fill=log10.f),
+    color=NA,
+    data=grid.dt)+
+  scale_fill_gradient(low="red", high="white")
+
+iseq <- seq(-4, 4, by=4)#for testing
+iseq <- seq(-4, 4, by=1)
+opt.vec.dt.list <- list()
+step.dt.list <- list()
+(initial.dt <- CJ(V1=iseq, V2=iseq, tau=seq(0.1, 0.4, by=0.1))[
+, x.start := sprintf("%.1f, %.1f", V1, V2)
+][])
+armijo.point.dt.list <- list()
+armijo.fun.dt.list <- list()
+for(param.i in 1:nrow(initial.dt)){
+  initial.row <- initial.dt[param.i]
+  x.start <- initial.row$x.start
+  tau <- initial.row$tau
+  #current.x <- c(-4.0, 0)
+  current.x <- initial.row[, c(V1, V2)]
+  cat(sprintf("%d / %d params %s tau=%.1f\n", param.i, nrow(initial.dt), x.start, tau))
+  step.num <- 0
+  grad.thresh <- 1e-8
+  armijo.steps <- 0
+  while({
+    norm.grad <- L2norm(c.vec <- xg(current.x))
+    step.dt.list[[paste(x.start, tau, step.num)]] <- data.table(
+      x.start, tau, step.num,  t(current.x), norm.grad, f=xf(current.x),
+      armijo.steps)
+    norm.grad > grad.thresh
+  }){
+    dir.vec <- -c.vec
+    min.log2.step <- -20
+    (step.size.vec <- 2^seq(0, min.log2.step))
+    armijo.slope <- c(tau*t(c.vec) %*% dir.vec)
+    armijo.intercept <- xf(current.x)
+    fstep <- function(step.vec){
+      do.call(f, as.data.table(t(matrix(dir.vec,2,length(step.vec))*matrix(step.vec, 2, length(step.vec), byrow=TRUE)+current.x)))
+    }
+    armijo.grid <- unique(sort(c(
+      step.size.vec,
+      2^seq(0, min.log2.step, l=200),
+      seq(0, 1, by=0.01))))
+    armijo.fun.list <- list(
+      objective=fstep,
+      bound=function(step.vec)armijo.intercept+step.vec*armijo.slope)
+    armijo.it.dt.list <- list()
+    for(Function in names(armijo.fun.list)){
+      fun <- armijo.fun.list[[Function]]
+      armijo.it.dt.list[[Function]] <- data.table(
+        Function, step.size=armijo.grid, f=fun(armijo.grid)
+      )[, f.thresh := ifelse(f<0, 0, f)][, let(
+        log2.step=log2(step.size),
+        log10.f=log10(f.thresh),
+        over=f>f[1])][]
+    }
+    n01 <- function(x)(x-min(x))/(max(x)-min(x))
+    norm01 <- function(over, under, f){
+      ok <- over==FALSE & under==FALSE
+      out <- f
+      out[ok] <- n01(f[ok])
+      fcase(
+        over, Inf,
+        under, -Inf,
+        default=out)
+    }
+    (armijo.it.dt <- rbindlist(armijo.it.dt.list)[
+    , under := f<min(f[Function=="objective"])
+    ][
+    , thresh.log10 := norm01(over, under, log10.f)
+    ][])
+    armijo.only <- armijo.it.dt[step.size %in% step.size.vec]
+    armijo.wide <- dcast(
+      armijo.only,
+      step.size + log2.step ~ Function,
+      value.var=c("f", "log10.f", "over","thresh.log10")
+    )[.N:1][, admissible := f_objective<f_bound][1:which(admissible)[1]]
+    armijo.steps <- nrow(armijo.wide)
+    ggplot()+geom_line(aes(step.size, f, color=Function), data=armijo.it.dt)
+    ggplot()+
+      geom_line(aes(log2.step, log10.f, color=Function), data=armijo.it.dt)+
+      geom_point(aes(log2.step, log10.f, color=Function), data=armijo.it.dt[under==FALSE & over==FALSE], shape=21, fill=NA)+
+      geom_point(aes(log2.step, log10.f_objective, fill=admissible), data=armijo.wide, shape=21)
+    ggplot()+
+      geom_line(aes(log2.step, thresh.log10, color=Function), data=armijo.it.dt)+
+      geom_point(aes(log2.step, thresh.log10, color=Function), data=armijo.it.dt[under==FALSE & over==FALSE], shape=21, fill=NA)+
+      geom_point(aes(log2.step, thresh.log10_objective, fill=admissible), data=armijo.wide, shape=21)
+    armijo.fun.dt.list[[paste(x.start, tau, step.num)]] <- data.table(
+      x.start, tau, step.num, armijo.it.dt)
+    armijo.point.dt.list[[paste(x.start, tau, step.num)]] <- data.table(
+      x.start, tau, step.num, armijo.wide)
+    armijo.step <- armijo.wide[.N, step.size]
+    step.vec <- dir.vec*armijo.step
+    new.x <- current.x+step.vec
+    opt.vec.list <- list(
+      Armijo.step=step.vec)
+    for(name in names(opt.vec.list)){
+      vec <- opt.vec.list[[name]]
+      opt.vec.dt.list[[paste(x.start, tau, step.num, name)]] <- data.table(
+        x.start, tau, step.num,  name, Vec(current.x+vec, current.x))
+    }
+    current.x <- new.x
+    step.num <- step.num+1
+  }
+}
+(opt.vec.dt <- rbindlist(opt.vec.dt.list))
+(step.dt <- rbindlist(step.dt.list))
+table(step.dt$armijo.steps)
+(armijo.fun.dt <- rbindlist(armijo.fun.dt.list))
+(armijo.point.dt <- rbindlist(armijo.point.dt.list))
+
+save(opt.vec.dt, step.dt, armijo.fun.dt, armijo.point.dt, grid.dt, initial.dt, file="figure-armijo-2D-data.RData")
--- figure-armijo-2D.R
+++ figure-armijo-2D.R
@@ -1,126 +1,182 @@
 library(data.table)
 library(animint2)
-f <- function(V1, V2){
-  (V1^2 + V2 - 11)^2 + (V1+V2^2-7)^2
-}
-xfun <- function(fun){
-  force(fun)
-  function(x)do.call(fun, as.list(x))
-}
-xf <- xfun(f)
-Vec <- function(to, from=c(0,0))data.table(to=t(to), from=t(from))
-grad <- function(x,y)c(
-  2*(x^2+y-11)*2*x+2*(x+y^2-7),
-  2*(x^2+y-11)+2*(x+y^2-7)*2*y)
-xg <- xfun(grad)
-hess <- function(x,y)rbind(
-  c(4*(x^2+y-11)+8*x^2+2, 4*x+4*y),
-  c(4*x+4*y, 4*(x+y^2-7)+8*y^2+2))
-xh <- xfun(hess)
-grad_at_d <- function(dir.vec)Q.mat %*% dir.vec + c.vec
-qd <- function(V1, V2){
-  V.mat <- cbind(V1, V2)
-  0.5*rowSums((V.mat %*% Q.mat) * V.mat) + V.mat %*% c.vec
-}
-xqd <- xfun(qd)
-L2norm <- function(x)sqrt(sum(x^2))
+load("figure-armijo-2D-data.RData")
+(initial.x.dt <- unique(initial.dt[, .(V1, V2, x.start)]))
+step.long <- melt(step.dt[, let(
+  "log10(|∇f|)"=log10(norm.grad),
+  "log10(f)"=log10(f),
+  "Armijo steps"=armijo.steps
+)], measure.vars = c("log10(|∇f|)", "log10(f)", "Armijo steps"))
+point.size <- 4
+scfill <- scale_fill_gradient(low="blue", high="white")
+(prev.dt <- step.dt[
+  step.dt,
+  .(x.start, tau, step.num=i.step.num, prev.step=x.step.num, V1, V2),
+  on=.(x.start, tau, step.num<=step.num)])
+end.dt <- step.dt[, .SD[.N], by=.(x.start, tau)]
+range.dt <- dcast(
+  end.dt,
+  x.start ~ .,
+  list(min, max),
+  value.var="step.num"
+)[
+, step.range := sprintf("%d–%d", step.num_min, step.num_max)
+][initial.x.dt, on="x.start"]
+ad.colors <- c(
+  "FALSE"="red",
+  "TRUE"="transparent")
+viz <- animint(
+  out.dir="figure-armijo-2D",
+  duration=list(step.num=500),
+  title="2D gradient descent with Armijo line search",
+  source="https://github.com/tdhock/2026-05-nlopt/blob/main/figure-armijo-2D.R",
+  steps=ggplot()+
+    ggtitle("Select step")+
+    theme_animint(width=500, height=500)+
+    scale_y_continuous("")+
+    scale_x_continuous(
+      "Step number",
+      breaks=seq(0, 100, by=10))+
+    make_tallrect(step.long, "step.num")+
+    geom_line(aes(
+      step.num, value, group=tau),
+      showSelected=c("x.start"),
+      clickSelects="tau",
+      size=3,
+      alpha_off=0.2,
+      alpha=1,
+      data=step.long)+
+    facet_grid(variable ~ ., scales="free"),
+  variables=ggplot()+
+    ggtitle("Cost f(x), select x start")+
+    scale_color_discrete("Vector")+
+    theme_animint(last_in_row=TRUE)+
+    theme_animint(width=500, height=500)+
+    grid.dt[, coord_cartesian(xlim=range(V1), ylim=range(V2))]+
+    geom_tile(aes(
+      V1, V2, fill=log10.f),
+      color=NA,
+      data=grid.dt)+
+    scfill+
+    scale_x_continuous("x1 = first optimization variable")+
+    scale_y_continuous("x2 = second optimization variable")+
+    geom_segment(aes(
+      from.V1, from.V2, xend=to.V1, yend=to.V2,
+      key=name, color=name),
+      showSelected=c("x.start","tau","step.num"),
+      data=opt.vec.dt)+
+    geom_point(aes(
+      to.V1, to.V2, color=name, key=name),
+      showSelected=c("x.start","tau","step.num"),
+      size=point.size,
+      data=opt.vec.dt)+
+    geom_point(aes(
+      V1, V2, color=name, key=1),
+      showSelected=c("x.start","tau","step.num"),
+      size=point.size,
+      data=data.table(name="iterate", step.dt))+
+    geom_text(aes(
+      V1, V2, label=armijo.steps, key=1),
+      showSelected=c("x.start","tau","step.num"),
+      data=data.table(name="iterate", step.dt))+
+    geom_path(aes(
+      V1, V2, key=1),
+      data=prev.dt,
+      showSelected=c("x.start", "tau", "step.num"))+
+    geom_point(aes(
+      V1, V2),
+      data=initial.x.dt,
+      size=7,
+      fill=NA,
+      clickSelects="x.start"),
+  ## armijo=ggplot()+
+  ##   geom_line(aes(
+  ##     step.size, ifelse(over, Inf, f), color=Function, group=Function, key=Function),
+  ##     showSelected=c("x.start","tau","step.num"),
+  ##     data=armijo.fun.dt)+
+  ##   scale_fill_manual(values=ad.colors)+
+  ##   geom_point(aes(
+  ##     step.size, ifelse(over_objective, Inf, f_objective), fill=admissible,
+  ##     key=step.size),
+  ##     showSelected=c("x.start","tau","step.num"),
+  ##     size=4,
+  ##     data=armijo.point.dt),
+  armijoLog=ggplot()+
+    ggtitle("Armijo line search for selected step")+
+    theme_animint(width=500, height=500)+
+    geom_line(aes(
+      log2.step, thresh.log10, color=Function, group=tau, key=tau),
+      showSelected=c("x.start","step.num"),
+      clickSelects="tau",
+      size=3,
+      data=armijo.fun.dt[Function=="bound"])+
+    geom_line(aes(
+      log2.step, thresh.log10, color=Function, group=Function, key=Function),
+      showSelected=c("x.start","tau","step.num"),
+      data=armijo.fun.dt)+
+    scale_fill_manual(values=ad.colors)+
+    scale_y_continuous("Log10(objective), normalized to [0,1]")+
+    geom_point(aes(
+      log2.step, thresh.log10_objective, fill=admissible, key=step.size),
+      showSelected=c("x.start","tau","step.num"),
+      size=4,
+      data=armijo.point.dt),
+  initx=ggplot()+
+    ggtitle("Select initial values")+
+    theme_animint(width=500, height=500)+
+    grid.dt[, coord_cartesian(xlim=range(V1), ylim=range(V2))]+
+    geom_tile(aes(
+      V1, V2, fill=log10.f),
+      color=NA,
+      data=grid.dt)+
+    scfill+
+    geom_path(aes(
+      V1, V2,
+      group=paste(x.start, tau)),
+      alpha=0.2,
+      size=1,
+      alpha_off=0.2,
+      clickSelects="x.start",
+      data=step.dt)+
+    geom_path(aes(
+      V1, V2,
+      key=tau,
+      group=tau),
+      data=step.dt,
+      size=4,
+      alpha=1,
+      alpha_off=0.2,
+      showSelected="x.start",
+      clickSelects="tau")+
+    geom_point(aes(
+      V1, V2,
+      key=tau,
+      group=tau),
+      data=end.dt,
+      size=4,
+      alpha=1,
+      alpha_off=0.5,
+      fill="violet",
+      showSelected="x.start",
+      clickSelects="tau")+
+    geom_point(aes(
+      V1, V2),
+      data=initial.x.dt,
+      size=7,
+      fill=NA,
+      clickSelects="x.start")+
+    geom_text(aes(
+      V1, V2, label=step.range),
+      data=range.dt,
+      color="red",
+      clickSelects="x.start"),
+  first=list(
+    x.start="-4.0, 0.0",
+    tau="0.1")
+)
 
-gseq <- seq(-5, 5, by=0.1)
-(grid.dt <- CJ(V1=gseq, V2=gseq)[
-, log10.f := log10(f(V1,V2)+1)
-][])
-ggplot()+
-  coord_equal()+
-  geom_tile(aes(
-    V1, V2, fill=log10.f),
-    color=NA,
-    data=grid.dt)+
-  scale_fill_gradient(low="red", high="white")
+#viz
 
-iseq <- seq(-4, 4, by=1)
-iseq <- seq(-4, 4, by=4)#for testing
-opt.vec.dt.list <- list()
-step.dt.list <- list()
-(initial.dt <- CJ(V1=iseq, V2=iseq, tau=seq(0.1, 0.4, by=0.1))[
-, x.start := sprintf("%.1f, %.1f", V1, V2)
-][])
-armijo.point.dt.list <- list()
-armijo.fun.dt.list <- list()
-for(param.i in 1:nrow(initial.dt)){
-  initial.row <- initial.dt[param.i]
-  x.start <- initial.row$x.start
-  tau <- initial.row$tau
-  #current.x <- c(-4.0, 0)
-  current.x <- initial.row[, c(V1, V2)]
-  cat(sprintf("%d / %d params %s tau=%.1f\n", param.i, nrow(initial.dt), x.start, tau))
-  step.num <- 0
-  grad.thresh <- 1e-8
-  armijo.steps <- 0
-  while({
-    norm.grad <- L2norm(c.vec <- xg(current.x))
-    step.dt.list[[paste(x.start, tau, step.num)]] <- data.table(
-      x.start, tau, step.num,  t(current.x), norm.grad, f=xf(current.x),
-      armijo.steps)
-    norm.grad > grad.thresh
-  }){
-    dir.vec <- -c.vec
-    min.log2.step <- -20
-    (step.size.vec <- 2^seq(0, min.log2.step))
-    armijo.slope <- c(tau*t(c.vec) %*% dir.vec)
-    armijo.intercept <- xf(current.x)
-    fstep <- function(step.vec){
-      do.call(f, as.data.table(t(matrix(dir.vec,2,length(step.vec))*matrix(step.vec, 2, length(step.vec), byrow=TRUE)+current.x)))
-    }
-    armijo.grid <- unique(sort(c(
-      step.size.vec,
-      2^seq(0, min.log2.step, l=200),
-      seq(0, 1, by=0.01))))
-    armijo.fun.list <- list(
-      objective=fstep,
-      bound=function(step.vec)armijo.intercept+step.vec*armijo.slope)
-    armijo.it.dt.list <- list()
-    for(Function in names(armijo.fun.list)){
-      fun <- armijo.fun.list[[Function]]
-      armijo.it.dt.list[[Function]] <- data.table(
-        Function, step.size=armijo.grid, f=fun(armijo.grid)
-      )[, f.thresh := ifelse(f<0, 0, f)][, let(
-        log2.step=log2(step.size),
-        log10.f=log10(f.thresh))][]
-    }
-    (armijo.it.dt <- rbindlist(armijo.it.dt.list))
-    armijo.only <- armijo.it.dt[step.size %in% step.size.vec]
-    armijo.wide <- dcast(
-      armijo.only,
-      step.size + log2.step ~ Function,
-      value.var=c("f", "log10.f")
-    )[.N:1][, admissible := f_objective<f_bound][1:which(admissible)[1]]
-    armijo.steps <- nrow(armijo.wide)
-    ggplot()+geom_line(aes(step.size, f, color=Function), data=armijo.it.dt)
-    ggplot()+
-      geom_line(aes(log2.step, log10.f, color=Function), data=armijo.it.dt)+
-      geom_point(aes(log2.step, log10.f_objective, fill=admissible), data=armijo.wide, shape=21)
-    armijo.fun.dt.list[[paste(x.start, tau, step.num)]] <- data.table(
-      x.start, tau, step.num, armijo.it.dt)
-    armijo.point.dt.list[[paste(x.start, tau, step.num)]] <- data.table(
-      x.start, tau, step.num, armijo.wide)
-    armijo.step <- armijo.wide[.N, step.size]
-    step.vec <- dir.vec*armijo.step
-    new.x <- current.x+step.vec
-    opt.vec.list <- list(
-      Armijo.step=step.vec)
-    for(name in names(opt.vec.list)){
-      vec <- opt.vec.list[[name]]
-      opt.vec.dt.list[[paste(x.start, tau, step.num, name)]] <- data.table(
-        x.start, tau, step.num,  name, Vec(current.x+vec, current.x))
-    }
-    current.x <- new.x
-    step.num <- step.num+1
-  }
+if(FALSE){
+  animint2pages(viz, "2026-06-08-armijo-2D", chromote_sleep_seconds=3)
 }
-(opt.vec.dt <- rbindlist(opt.vec.dt.list))
-(step.dt <- rbindlist(step.dt.list))
-table(step.dt$armijo.steps)
-(armijo.fun.dt <- rbindlist(armijo.fun.dt.list))
-(armijo.point.dt <- rbindlist(armijo.point.dt.list))
-
-save(opt.vec.dt, step.dt, armijo.fun.dt, armijo.point.dt, grid.dt, file="figure-armijo-2D-data.RData")
